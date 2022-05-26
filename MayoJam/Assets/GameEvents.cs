99de761e50503db89c7da9a0134696071a3f249f
Matias <matias@karplus.com.ar>
2022-05-25 22:46:44 -0300

    Added destroy on play to particles

--- MayoJam/Assets/GameEvents.cs
+++ MayoJam/Assets/GameEvents.cs
@@ -10,9 +10,11 @@ public class GameEvents : MonoBehaviour
     public CoinBehaviour[] coins;
     public TMP_Text scoreText;
     private int score;
+    public ParticleSystem bloodSpat;
 
     public static GameEvents current;
     public static Action<Enemy> OnEnemyKilled;
+    
     //public event Func<int,int, bool> ReturnCringe;
     
     private void Awake(){
@@ -40,8 +42,8 @@ public class GameEvents : MonoBehaviour
     }
 
     private void CreateBloodSpatter(Enemy e) {
-        Debug.Log(e);
-        //Instantiate Particles
+        Instantiate(bloodSpat, e.transform.position, Quaternion.Euler(-90,0,0));
+
     }
 
     private void TriggerScoreUI() {
